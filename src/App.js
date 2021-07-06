e88d9ba3c356fcf0b6eebb6aec5a9b1967151cdf
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-07-06 13:24:29 +0200

    Add router and switch / import pages and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// Components
+
+import BottomMenu from "./components/bottommenu/BottomMenu";
+
+// Pages
+
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import OverviewPage from "./pages/Overview";
+import DetailPage from "./pages/Detail";
+import AboutPage from "./pages/About";
+import ContactPage from "./pages/Contact";
+import ProfilePage from "./pages/Profile";
+
+// CSS
+
+import "./App.css";
+
+// React Router
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<HomePage />
+				</Route>
+				<Route exact path="/login">
+					<LoginPage />
+				</Route>
+				<Route exact path="/overview">
+					<OverviewPage />
+				</Route>
+				<Route exact path="/overview/:dog">
+					<DetailPage />
+				</Route>
+				<Route exact path="/about">
+					<AboutPage />
+				</Route>
+				<Route exact path="/contact">
+					<ContactPage />
+				</Route>
+				<Route exact path="/profile">
+					<ProfilePage />
+				</Route>
+			</Switch>
+			<BottomMenu />
+		</Router>
+	);
 }
 
 export default App;
