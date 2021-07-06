3fb23ed8e1f9fab1afb1b21837d7e738b526bf45
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-07-06 14:21:23 +0200

    Add default CSS and import icons in BottomMenu.js

--- src/components/bottommenu/BottomMenu.js
+++ src/components/bottommenu/BottomMenu.js
@@ -1,42 +1,54 @@
+// React Router
+
 import { NavLink } from "react-router-dom";
 
-function BottomMenu() {
+// Icons
+
+import {
+	HiUser,
+	HiSearch,
+	HiHome,
+	HiInformationCircle,
+	HiChatAlt2,
+} from "react-icons/hi";
+
+const BottomMenu = () => {
 	return (
 		<nav>
 			<div className="nav-container">
 				<ul>
 					<li>
 						<NavLink to="/profile" exact activeClassName="active-link">
-							Profile
+							<HiUser />
 						</NavLink>
 					</li>
 
 					<li>
 						<NavLink to="/overview" exact activeClassName="active-link">
-							Overview
+							<HiSearch />
 						</NavLink>
 					</li>
 
 					<li>
 						<NavLink to="/" exact activeClassName="active-link">
-							Home
+							<HiHome />
 						</NavLink>
 					</li>
 
 					<li>
 						<NavLink to="/about" exact activeClassName="active-link">
-							About
+							<HiInformationCircle />
 						</NavLink>
 					</li>
 					<li>
 						<NavLink to="/contact" exact activeClassName="active-link">
-							Contact
+							<HiChatAlt2 />
 						</NavLink>
 					</li>
 				</ul>
 			</div>
 		</nav>
 	);
-}
+};
 
 export default BottomMenu;
