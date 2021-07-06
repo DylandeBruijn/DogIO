79f7512cfeba0e2917c7dcec1ee02d0e3c040f2a
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-07-06 17:32:55 +0200

    Restructure PrivateRoute function as component

---
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,13 @@
+// React Router
+
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ children, isAuthenticated, ...rest }) => {
+	return (
+		<Route {...rest}>
+			{isAuthenticated ? children : <Redirect to="/login" />}
+		</Route>
+	);
+};
+
+export default PrivateRoute;
