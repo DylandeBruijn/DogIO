10a32efc769cdda3d335301ddc75ef187ad982de
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-07-08 12:42:09 +0200

    Cleanup CSS and imports of BottomMenu

--- src/components/bottommenu/BottomMenu.jsx
+++ src/components/bottommenu/BottomMenu.jsx
@@ -1,9 +1,5 @@
-// React Router
-
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-
-// React Icons
-
 import { IconContext } from "react-icons";
 import {
 	HiUser,
@@ -12,72 +8,69 @@ import {
 	HiInformationCircle,
 	HiChatAlt2,
 } from "react-icons/hi";
-
-// CSS
-
 import styles from "./BottomMenu.module.scss";
 
 const BottomMenu = () => {
 	return (
-		<nav className={styles["nav"]}>
+		<nav className={styles["Nav"]}>
 			<NavLink
 				to="/profile"
 				exact
-				className={styles["nav__link"]}
-				activeClassName={styles["nav__link--active"]}
+				className={styles["Nav__link"]}
+				activeClassName={styles["Nav__link--active"]}
 			>
 				<IconContext.Provider value={{ size: "2em" }}>
 					<HiUser />
 				</IconContext.Provider>
-				<span className={styles["nav__text"]}>Profile</span>
+				<span className={styles["Nav__text"]}>Profile</span>
 			</NavLink>
 
 			<NavLink
 				to="/overview"
 				exact
-				className={styles["nav__link"]}
-				activeClassName={styles["nav__link--active"]}
+				className={styles["Nav__link"]}
+				activeClassName={styles["Nav__link--active"]}
 			>
 				<IconContext.Provider value={{ size: "2em" }}>
 					<HiSearch />
 				</IconContext.Provider>
-				<span className={styles["nav__text"]}>Overview</span>
+				<span className={styles["Nav__text"]}>Overview</span>
 			</NavLink>
 
 			<NavLink
 				to="/"
 				exact
-				className={styles["nav__link"]}
-				activeClassName={styles["nav__link--active"]}
+				className={styles["Nav__link"]}
+				activeClassName={styles["Nav__link--active"]}
 			>
 				<IconContext.Provider value={{ size: "2em" }}>
 					<HiHome />
 				</IconContext.Provider>
-				<span className={styles["nav__text"]}>Home</span>
+				<span className={styles["Nav__text"]}>Home</span>
 			</NavLink>
 
 			<NavLink
 				to="/about"
 				exact
-				className={styles["nav__link"]}
-				activeClassName={styles["nav__link--active"]}
+				className={styles["Nav__link"]}
+				activeClassName={styles["Nav__link--active"]}
 			>
 				<IconContext.Provider value={{ size: "2em" }}>
 					<HiInformationCircle />
 				</IconContext.Provider>
-				<span className={styles["nav__text"]}>About</span>
+				<span className={styles["Nav__text"]}>About</span>
 			</NavLink>
 
 			<NavLink
 				to="/contact"
 				exact
-				className={styles["nav__link"]}
-				activeClassName={styles["nav__link--active"]}
+				className={styles["Nav__link"]}
+				activeClassName={styles["Nav__link--active"]}
 			>
 				<IconContext.Provider value={{ size: "2em" }}>
 					<HiChatAlt2 />
 				</IconContext.Provider>
-				<span className={styles["nav__text"]}>Contact</span>
+				<span className={styles["Nav__text"]}>Contact</span>
 			</NavLink>
 		</nav>
 	);
