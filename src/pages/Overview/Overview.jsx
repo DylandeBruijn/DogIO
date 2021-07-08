ed1e187cfa4cd60df1a00522f36e825ccf3ca1da
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-07-08 12:43:09 +0200

    Implement first version of API integration

--- src/pages/Overview/Overview.jsx
+++ src/pages/Overview/Overview.jsx
@@ -1,20 +1,43 @@
-// CSS
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import DogFilter from "../../components/DogFilter/DogFilter";
 import styles from "./Overview.module.scss";
 
 const Overview = () => {
+	const [dogData, setDogData] = useState(null);
+	const [error, setError] = useState("");
+	const [loading, toggleLoading] = useState(false);
+
+	useEffect(() => {
+		async function fetchData() {
+			setError(false);
+			toggleLoading(true);
+			try {
+				const response = await axios.get(
+					`https://api.thedogapi.com/v1/breeds?api_key=cb9794bd-ce01-481d-947a-b4e2c981c7c0`
+				);
+				setDogData(response.data);
+				console.log(response.data);
+			} catch (e) {
+				console.error(e);
+				setError(true);
+			}
+			toggleLoading(false);
+		}
+		fetchData();
+	}, []);
 	return (
 		<section className={styles["Page"]}>
-			<div className={styles["Page__search"]}>
-				<input type="text" className={styles["Page__search__field"]}></input>
-			</div>
-			<div className={styles["Page__filter"]}>
-				<h1 className={styles["Page__filter__title"]}>Start searching</h1>
-				<p className={styles["Page__filter__body"]}>
-					More than a hundred different dog breeds
-				</p>
-				<div></div>
-			</div>
+			{error && <span>Oeps er is iets fout gegaan!</span>}
+			{loading && <span>Loading...</span>}
+			{dogData?.map((dog) => (
+				<article>
+					<h3>Name: {dog.name}</h3>
+					<p>Bred for: {dog.bred_for}</p>
+					<p>Breed group: {dog.breed_group}</p>
+				</article>
+			))}
 		</section>
 	);
 };
